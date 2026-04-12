d6780bad81bb81299fa2684aebac6f64239959a8
RanaPranav22 <pranavrana019@gmail.com>
2026-04-12 21:44:31 +0530

    ragstate and vectorstore bugs fixed

--- src/state/rag_state.py
+++ src/state/rag_state.py
@@ -6,4 +6,38 @@ from langchain.schema import Document
 class RAGState(BaseModel):
     question: str
     retrived_docs = List[Document] = []
+
     answer: str = ""
+
+    # from typing import List
+    # from pydantic import BaseModel, Field
+    # from langchain_core.documents import Document
+
+    # class RAGState(BaseModel):
+    #     question: str
+    #     retrieved_docs: List[Document] = Field(default_factory=list)
+    #     answer: str = ""
+
+    """    FLOW :-
+            Initial State:
+            ----------------
+            question = "What is AI?"
+            retrieved_docs = []
+            answer = ""
+
+                    ↓
+
+            After Retriever Node:
+            ----------------
+            question = "What is AI?"
+            retrieved_docs = [doc1, doc2]
+            answer = ""
+
+                    ↓
+
+            After LLM Node:
+            ----------------
+            question = "What is AI?"
+            retrieved_docs = [doc1, doc2]
+            answer = "AI is..."
+    """
